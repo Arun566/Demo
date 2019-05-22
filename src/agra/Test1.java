baac102dbe6173ba8d70fe9d09a590e1f1769df6
user <user@192.168.100.4>
2019-05-23 00:15:08 +0530

    Open ie browser

---
+++ src/agra/Test1.java
@@ -0,0 +1,19 @@
+package agra;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\arun\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		
+		
+
+		
+		
+		}
+
+}
